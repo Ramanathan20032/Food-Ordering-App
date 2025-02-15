fa5e4329570def996e6f77b66972c8168a28df2c
Ramanathan20032 <Ramanathan20032@gmail.com>
2025-02-15 16:23:51 +0530

    15/02 : useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockdata";
+import { useState } from "react";
 
 // Body Component
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurant = listOfRestaurant.filter(
+              (res) => res.info.avgRating >= 4.7
+            );
+            setListOfRestaurant(filteredRestaurant)
+          }}
+        >
+          Above 4.7
+        </button>
       </div>
       <div className="res-container">
-        {
-          resList.map((restaurant) => (
-            <RestaurantCard key={restaurant.info.id} resData = {restaurant}/>
-          ))
-        }
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Body;
