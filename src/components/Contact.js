208ee13ac50df58ab61bb129335b2f8ede5c4385
Ramanathan20032 <ramanathan.kr@maghil.com>
2025-07-05 16:14:55 +0530

    basic Testing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 const Contact = () => {
-  return (
-    <div className="p-5">
-      <p className="text-xl text-center font-bold mb-3">Contact Us Page</p>
+  return(
+    <div className="max-w-3xl mx-auto shadow-md rounded-xl m-7 p-5">
+      <h1 className="text-4xl text-center font-semibold capitalize text-blue-950 mb-3">contact Us</h1>
+      <p className="text-lg text-center text-amber-950">Contact Page Content</p>
     </div>
-  );
+  )
 };
 
 export default Contact;
